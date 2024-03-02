ac414fedd62de58339de63f2837a26b2ea571d32
Aditya Kakarla <adityakakarla@Adityas-Air.attlocal.net>
2024-03-02 10:03:34 -0800

    added auth

--- app.py
+++ app.py
@@ -2,11 +2,28 @@ from flask import Flask
 from flask_restful import Api, Resource
 import requests
 from bs4 import BeautifulSoup
+from flask_httpauth import HTTPBasicAuth
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 api = Api(app)
+auth = HTTPBasicAuth()
+load_dotenv()
+user = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+
+users = {
+    user: password
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and users[username] == password:
+        return username
 
 class Schedule(Resource):
+    @auth.login_required
     def get(self):
         url = 'https://www.formula1.com/en/racing/2024.html'
         response = requests.get(url)
